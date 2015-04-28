987f4d32f6aecfbeca35d8da75a888f638920e2c
Sergio <huelva91@gmail.com>
2015-04-29 00:53:19 +0200

    H

--- src/Cerillas.java
+++ src/Cerillas.java
@@ -1,12 +1,20 @@
+import java.util.Scanner;
+
 /**
  * Created by sergio on 28/04/15.
  */
 public class Cerillas {
 
+
+
     public static void main(String [] arg){
 
+        Scanner entradaEscaner = new Scanner(System.in);
+        String cerillas;
         System.out.println("Bienvenido al juego de las cerilas");
         System.out.println("¿Cuántas cerillas hay en la fila?:");
+        cerillas = entradaEscaner.nextLine();
+        System.out.printf("La entrada recibida es " + cerillas);
 
     }
 }
