86810482945125e1680c31618b4bfc2b86de761d
Sergio <huelva91@gmail.com>
2015-05-05 09:52:33 +0200

    Control entrada cerillas

--- src/Cerillas.java
+++ src/Cerillas.java
@@ -10,11 +10,23 @@ public class Cerillas {
     public static void main(String [] arg){
 
         Scanner entradaEscaner = new Scanner(System.in);
-        String cerillas;
+        int cerillas;
+        int quitar;
+        String turno;
         System.out.println("Bienvenido al juego de las cerilas");
         System.out.println("¿Cuántas cerillas hay en la fila?:");
-        cerillas = entradaEscaner.nextLine();
+        cerillas = entradaEscaner.nextInt();
         System.out.println("La entrada recibida es " + cerillas);
+        System.out.println("¿Cuantas cerillas quieres quitar? (1,2 o 3)");
+        quitar = entradaEscaner.nextInt();
+        while (quitar > 3 || quitar < 1 ){
+
+            System.out.println("Error introduce una cantidad de cerillas a quitar adecuada");
+            quitar = entradaEscaner.nextInt();
+
+
+        }
+
 
     }
 }
