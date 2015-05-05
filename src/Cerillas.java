b84009511e105a9c81bc5ac13694aafac3db0e3d
Sergio <huelva91@gmail.com>
2015-05-05 19:24:36 +0200

    Implementar turnos

--- src/Cerillas.java
+++ src/Cerillas.java
@@ -18,20 +18,30 @@ public class Cerillas {
         Scanner entradaEscaner = new Scanner(System.in);
         int cerillas;
         int quitar;
-        String turno;
+        boolean turnoP = false;
         System.out.println("Bienvenido al juego de las cerilas");
         System.out.println("¿Cuántas cerillas hay en la fila?:");
         cerillas = entradaEscaner.nextInt();
-        while (cerillas != 0) {
-            System.out.println("¿Cuantas cerillas quieres quitar? (1,2 o 3)");
-            quitar = entradaEscaner.nextInt();
+        while (cerillas > 0) {
+            System.out.println("Quedan..." + cerillas);
+            if (turnoP == false){
+
+                System.out.println("¿Cuantas cerillas quieres quitar? M (1,2 o 3)");
+                quitar = entradaEscaner.nextInt();
+
+            }
+            else {
+                System.out.println("¿Cuantas cerillas quieres quitar? P (1,2 o 3)");
+                quitar = entradaEscaner.nextInt();
+            }
             while (quitar > 3 || quitar < 1) {
+
 
                 System.out.println("Error introduce una cantidad de cerillas a quitar adecuada");
                 quitar = entradaEscaner.nextInt();
-                cerillas = cerillas - quitar;
             }
-            System.out.println("Quedan..." + cerillas);
+            cerillas = cerillas - quitar;
+
         }
     }
 }
